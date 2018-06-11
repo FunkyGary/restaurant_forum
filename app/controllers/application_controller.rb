d9252fddbc49633468c41d07995831f0022c743c
gary80212@gmail.com <gary80212@gmail.com>
2018-06-12 01:19:06 +0800

    setup restaurant for public users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow!"
-      redirect_to root_path
-    end
-  end
 end
