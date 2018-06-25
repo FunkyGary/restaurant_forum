10c26393c20ba0aa4b6fec25e955bc4fb9b7bd89
gary80212@gmail.com <gary80212@gmail.com>
2018-06-26 01:10:43 +0800

    heroku update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "user was successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "user was failed to update"
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :intro, :avatar)
+  end
+
+end
