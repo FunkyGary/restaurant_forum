5b6e57fac5f179e51355c8b787f1167e61f32156
gary80212@gmail.com <gary80212@gmail.com>
2018-07-01 23:37:53 +0800

    create like model and srtup relation among user, restaurant and like

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,10 @@ class Restaurant < ApplicationRecord
   has_many :commets, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+  has_many :likes, dependent: :destroy
+  has_many :likes_users, through: :likes, source: :user
+
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates_presence_of :name
+  validates_presence_of :name, :email
   mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -14,4 +14,8 @@ class User < ApplicationRecord
   # 「使用者收藏很多餐廳」的多對多關聯
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
+
 end
